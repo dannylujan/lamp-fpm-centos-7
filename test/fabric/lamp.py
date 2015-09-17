775d4206bf3c5ace46a5a54776e0e69ad2e139d2
Randall Burt <randall.burt@rackspace.com>
2015-09-17 17:36:23 -0500

    Fix tests to not need lsb

--- test/fabric/lamp.py
+++ test/fabric/lamp.py
@@ -32,18 +32,15 @@ def apache_is_responding():
 @task
 def check():
     env.platform_family = detect.detect()
-    release = run("/usr/bin/lsb_release -r | awk {'print $2'}")
 
     if env.platform_family == "rhel":
-        if float(release) < 7:
-            print "RHEL/Cent 6.x"
-            assert package.installed("httpd"), "httpd not installed"
-            assert package.installed("holland"), "holland is not installed"
-            assert package.installed("mysql55"), "mysql55 is not insalled"
-            assert process.is_up("httpd"), "process httpd not running"
-            assert service.is_enabled("httpd"), "httpd not enabled"
-            # welcome.conf causes a 403 when running apache_is_responding()
-            # with the stock build.
+        assert package.installed("httpd"), "httpd not installed"
+        assert package.installed("holland"), "holland is not installed"
+        assert package.installed("mysql55"), "mysql55 is not insalled"
+        assert process.is_up("httpd"), "process httpd not running"
+        assert service.is_enabled("httpd"), "httpd not enabled"
+        # welcome.conf causes a 403 when running apache_is_responding()
+        # with the stock build.
 
     if env.platform_family == "debian":
         print "Ubuntu 12.04/14.04 or Debian 7.x/8.x"
